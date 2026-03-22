b6ef9006fc99cb5b6925968aba1a205ec5a0ff80
Anish Palacherla <anishpalacherla@Anishs-MacBook-Air.local>
2026-03-22 21:30:27 +0530

    UI upgraded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,36 +5,31 @@ function App() {
 
   const getSlots = async () => {
     try {
-      const res = await fetch("http://127.0.0.1:8000/free-slots");
+      const res = await fetch("http://192.168.1.4:8000/free-slots");
       const data = await res.json();
-      console.log(data); // 👈 IMPORTANT (check console)
       setSlots(data);
     } catch (error) {
-      console.error("Error fetching slots:", error);
+      console.error("Error:", error);
     }
   };
 
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Free Slots</h1>
+      <h1>OpenSlots</h1>
 
       <button onClick={getSlots}>Get Free Slots</button>
 
-      {slots.length === 0 ? (
-        <p>No data yet</p>
-      ) : (
-        slots.map((dayData, index) => (
-          <div key={index} style={{ marginTop: "20px" }}>
-            <h2>{dayData.day}</h2>
+      {slots.map((dayData, index) => (
+        <div key={index}>
+          <h2>{dayData.day}</h2>
 
-            {dayData.free_slots.map((slot, i) => (
-              <div key={i}>
-                ⏰ {slot.start} - {slot.end}
-              </div>
-            ))}
-          </div>
-        ))
-      )}
+          {dayData.free_slots.map((slot, i) => (
+            <p key={i}>
+              {slot.start} - {slot.end}
+            </p>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
